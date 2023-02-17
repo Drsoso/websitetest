dbf23dd506887ef1629ab4099214f3d03c3260b7
Drsoso <kechkwir01@gmail.com>
2023-02-17 23:58:52 +0100

    test 13

--- docsvoicetest/settings.py
+++ docsvoicetest/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'docsvoicetest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,8 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
