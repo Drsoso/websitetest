f99b556e7b35a019ef306eb9fa673f6499f7e16e
Drsoso <kechkwir01@gmail.com>
2023-02-17 19:29:32 +0100

    test4

--- docsvoicetest/settings.py
+++ docsvoicetest/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'docsvoicetest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(MAIN_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'docsvoicetest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': MAIN_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+print ("base dir path", BASE_DIR)
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -127,10 +127,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(MAIN_DIR,'docsvoicetest/static'),
+    os.path.join(BASE_DIR,'docsvoicetest/static'),
 )
 
-STATIC_ROOT = os.path.join(MAIN_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
